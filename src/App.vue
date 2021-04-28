56a1a7586bdf3e091659968101e1b06f41b0aa80
Orodan <jimmykasprzak@gmail.com>
2021-04-28 10:56:37 +0200

    🔥 remove typo from app title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Great Your Vue.js App from"/>
+    <HelloWorld msg="Welcome to Great Your Vue.js App"/>
   </div>
 </template>
 
